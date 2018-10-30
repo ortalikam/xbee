59686059e0fd807526b20b233cd89245fb27532a
ortal kaminka <ortalkaminka@gmail.com>
2018-10-30 17:36:30 +0200

    adding constants and state machine to loop

--- src/Constants.h
+++ src/Constants.h
@@ -1,5 +1,24 @@
 #pragma once
 
+//msg
+#define BUILD_NETWORK 1
+#define GET_REQUEST 2
+#define SET_REQUEST 3
+#define GET_RESPONSE 4
+#define TRAP 5
+#define NETWORK 6
+
+//parameters
+//parameters
+#define TIME_TO_REBUILD_NETWORK 60000 // every 2 minthe minutes 
+#define NUMBER_ATTEMPTS_SEND_MESSAGE 2
+#define TIME_TO_GET_ACK 5000 // wait up to 5 seconds for response
+
+#define PARAM_TIME_TO_REBUILD_NETWORK 1
+#define PARAM_NUMBER_ATTEMPTS_SEND_MESSAGE 2
+#define PARAM_TIME_TO_GET_ACK 3
+
+
 #define REQUEST_ID_SIZE 2
 #define KEY_SIZE 2
 #define PARAM_DATA_SIZE 50 
@@ -10,3 +29,5 @@
 #define TRAP_SIZE 11 //6+1+5+max 32 param * 2 bytes
 #define NETWORK_SIZE 76 //6+1+2+1+2++max 32 param * 2 bytes
 #define MAX_SIZE 76 
+#define SON_SIZE 10
+#define FATHERS_SIZE 10
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,34 @@
 #include <XBee.h>
 #include "network.h"
-
+#include "Constants.h"
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
 // create reusable response objects for responses we expect to handle 
 Rx16Response rx16 = Rx16Response();
-Rx64Response rx64 = Rx64Response();
- 
-Network network;
+
+ Network network;
+
+//Buffer for receiving Serial data
+uint8_t serialData[100] ;
+
+///uint8_t* data  ;
+
+struct dataStruct { 
+  uint32_t sons[SON_SIZE]; // array of suns
+  int sons_size=0; //how much sons exists
+
+  uint32_t fathers[FATHERS_SIZE]; // array of fathers
+  int father_size=0; //how much fathers exists
+
+  //parameters
+  int time_to_rebuild_network; //define-1 , in microsecond
+  int Number_attempts_send_message; //define-2 
+  int Time_to_get_ack; //define-3 ,in microsecond 
+
+  // מערך ציקלי של הודעות 
+}myData;
 
 
-uint8_t option = 0;
-uint8_t* data  ;
 
 void setup() {
   
@@ -19,16 +36,24 @@ void setup() {
   Serial1.begin(9600);
   Serial.begin(9600);
   xbee.setSerial(Serial1);
-  
-  network=Network(xbee,Serial);
-
+ network=Network(xbee,Serial);
 }
 
 // continuously reads packets, looking for RX16 or RX64
 void loop() {
-    Serial.println("waiting..");
-
     
+    //serial
+    if (Serial.available()) {
+      Serial.readBytes(serialData, sizeof(serialData));
+
+    if (serialData[0]==BUILD_NETWORK)  find_Neighbors();
+
+    else if (serialData[0]=SET_REQUEST)  set_request();
+
+    else if (serialData[0]=GET_RESPONSE)  get_request();
+   }
+
+  //xbee
     xbee.readPacket();
     if (xbee.getResponse().isAvailable()) {
       // got something
@@ -40,7 +65,10 @@ void loop() {
                   Serial.println("Broadcast");
                 else
                  Serial.println("not Broadcast");
-        	data = rx16.getData();
+          option = rx16.getOption();
+          Serial.print("option");
+          Serial.println(option);
+
            for (int i = 0; i < rx16.getFrameDataLength(); i++) {
             Serial.print("FrameData [");
             Serial.print(i, DEC);
@@ -68,4 +96,40 @@ void loop() {
       // or flash error led
     } 
       delay(1000);
+}
+
+bool find_Neighbors() {
+  String ids[SONSIZE];
+  int idsNum = 0;
+
+  if (!writeToWireless(addressTx, message[0])) { //sending "discover Neighbors" message
+    //writeToSerial("error in sending find_Neighbors");
+    return false;
+  }
+   
+  radio.startListening();
+  delay(100);
+  while (radio.available(addressRx)) {
+    //delay(100);
+    char text[50] = "";
+    radio.read(&text, sizeof(text));
+    String newText(text);
+    //writeToSerial(newText);
+    String msg = newText.substring(0, strlen(message[1]));
+    if (msg == message[1]) //id_
+    {
+      String idNumber = newText.substring(strlen(message[1]), strlen(text));
+      ids[idsNum++] = idNumber;
+      //writeToSerial(idNumber);
+      
+    }
+    delay(1000);
+  }
+  for (int i=0;i<idsNum;i++)
+    if (!handShake(ids[i])) {
+      writeToSerial("error in handShake");
+      return false;
+    }
+  return true;
+
 }
